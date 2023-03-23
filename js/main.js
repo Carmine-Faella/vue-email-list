e497d8ce91e525c704412c8b583412691764de85
Carmine Faella <carminefaella5@gmail.com>
2023-03-23 14:49:28 +0100

    Esercizio Completato

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      email: [],
+    }
+  },
+  mounted() {
+
+    for(let i = 0; i<10; i++){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then( (response) => {
+           this.email.push(response.data.response);
+           console.log(this.email)
+        }
+    );
+    }
+
+  }
+}).mount('#app')
